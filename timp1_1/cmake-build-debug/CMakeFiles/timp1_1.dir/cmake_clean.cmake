0083f96eb17b76123354cef0407f86455691ac4c
Kateika-M <muromskaya.katya@mail.ru>
2021-04-23 22:52:01 +0300

    Add laboratory work 1

---
+++ timp1_1/cmake-build-debug/CMakeFiles/timp1_1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/timp1_1.dir/main.cpp.o"
+  "CMakeFiles/timp1_1.dir/modAlphaCipher.cpp.o"
+  "timp1_1"
+  "timp1_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/timp1_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ timp1_2/cmake-build-debug/CMakeFiles/timp1_2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/timp1_2.dir/main.cpp.o"
+  "CMakeFiles/timp1_2.dir/modAlphaCipher.cpp.o"
+  "timp1_2"
+  "timp1_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/timp1_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
